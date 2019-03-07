b68723935b95a96b4cbbb84ccd68f431aed31991
Roderick <notmine@rjbradley.local>
2019-03-06 20:50:14 -0600

    completed exercise-04

--- exercise-04.py
+++ exercise-04.py
@@ -13,9 +13,55 @@
 # 3. Print a message such as:
 #      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
 
+
+
+# This was such a pain build and an eye sour to look at
+# Set a to an empty list
+print("Please enter the three lengths of a triangle (one at a time)")
+
+a = []
+
 def triangle():
-  my_tri = {1: 'Length 1', 2: 'Lenght 2', 3: 'Lenght 3'}
-  for key, value in enumerate(my_tri, 1):
-    if key != 4:
-      key = input(f"enter length of triangle for side {value} = ")
+    
+    prompt = int(input('a: '))
+    push_to_list = a.append(prompt) 
+    
+    while a == []:push_to_list
+    prompt1 = prompt
+    
+    if a != []:prompt = int(input('b: '))
+    push_to_list 
+    prompt2 = prompt
+    
+    if a != []:prompt = int(input('c: '))
+    push_to_list
+    prompt3 = prompt
+    
+    if a != []: 
+        if prompt1 == prompt2 or prompt1 == prompt3 or prompt2 == prompt3 :
+            tri_type = 'equalateral'
+    
+    if a != []:
+        if prompt1 != prompt2 or prompt1 != prompt3 or prompt2 != prompt3 :
+            tri_type = 'scalene'
+            
+    if a != []:
+        if prompt1 != prompt2 or prompt1 == prompt2 or prompt1 == prompt3 :
+            tri_type = 'isosceles'
+    
+    if a != []:
+        if prompt1 == prompt2 or prompt1 == prompt2 or prompt1 != prompt3 :
+            tri_type = 'isosceles'
+            
+    print(f"A triangle with sides of {prompt1}, {prompt2} & {prompt3} is a {tri_type}  triangle")
+    
 triangle()
+
+
+
+# def triangle():
+#   my_tri = {1: 'Length 1', 2: 'Lenght 2', 3: 'Lenght 3'}
+#   for key, value in enumerate(my_tri, 1):
+#     if key != 4:
+#       key = input(f"enter length of triangle for side {value} = ")
+# triangle()
